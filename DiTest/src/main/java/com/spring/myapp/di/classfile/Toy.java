e899fc67f91c72ab26e01bcfb08dd6471ba549fb
Ryu0408 <fbtmdghks11@naver.com>
2020-01-06 11:32:48 +0900

    Spring 오류 시 JavaSE-1.8 확인!!

--- DiTest/src/main/java/com/spring/myapp/di/classfile/Toy.java
+++ DiTest/src/main/java/com/spring/myapp/di/classfile/Toy.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class Toy {
 	
 	@Autowired
-	@Qualifier("DuraBattery")
+	//@Qualifier("DuraBattery")
 	IBattery battery;
 	
 	public void playToy() {
