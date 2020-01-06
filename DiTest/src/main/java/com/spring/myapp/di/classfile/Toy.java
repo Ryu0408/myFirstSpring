186d0d4141d9662984ceb6c0e7010da12110539d
Ryu0408 <fbtmdghks11@naver.com>
2020-01-06 11:51:24 +0900

    Autowired, Qualifier 2번쓰기 귀찮음 Resource로 실행

--- DiTest/src/main/java/com/spring/myapp/di/classfile/Toy.java
+++ DiTest/src/main/java/com/spring/myapp/di/classfile/Toy.java
@@ -1,5 +1,7 @@
 package com.spring.myapp.di.classfile;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -7,9 +9,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class Toy {
 	
-	@Autowired
-	//@Qualifier("DuraBattery")
+	//@Autowired
+	//@Qualifier("enerBattery")
+	@Resource(name="duraBattery")
 	IBattery battery;
+
 	
 	public void playToy() {
 		System.out.println("장난감을 가지고 놉니다.");
