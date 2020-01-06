4e0f3c4a86e0eecdc434a07276e93f72be113c61
Ryu0408 <fbtmdghks11@naver.com>
2020-01-06 10:43:59 +0900

    의존성 연습

--- myFirstSpring/src/main/java/com/spring/myapp/di/classfile/Ballad.java
+++ myFirstSpring/src/main/java/com/spring/myapp/di/classfile/Ballad.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Ballad implements Singer{
 	
+	@Override
 	public void sing() {
 		System.out.println("발라드가수가 소몰이를 합니다.");
 	}
