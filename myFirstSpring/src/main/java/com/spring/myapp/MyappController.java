f82657ab01cb34dbe3cf7294bd6ee8e00f3ed5e0
Ryu0408 <fbtmdghks11@naver.com>
2020-01-07 17:15:24 +0900

    POST, GET Mapping 하는법

--- myFirstSpring/src/main/java/com/spring/myapp/MyappController.java
+++ myFirstSpring/src/main/java/com/spring/myapp/MyappController.java
@@ -3,6 +3,7 @@ package com.spring.myapp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyappController {
@@ -10,5 +11,10 @@ public class MyappController {
 	@RequestMapping(value="goA")
 	public String goA(Model model) {
 		return "A";
+	}
+	
+	@RequestMapping(value="goB", method=RequestMethod.POST)
+	public String goB(Model model) {
+		return "B";
 	}
 }
