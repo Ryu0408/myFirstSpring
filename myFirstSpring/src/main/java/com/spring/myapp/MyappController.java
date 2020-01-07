3261287f3d3a95360580d136a814854329f99ed0
Ryu0408 <fbtmdghks11@naver.com>
2020-01-07 23:58:02 +0900

    form구성방법과 Spring에서의 form에서 form으로 이동방법

--- myFirstSpring/src/main/java/com/spring/myapp/MyappController.java
+++ myFirstSpring/src/main/java/com/spring/myapp/MyappController.java
@@ -19,4 +19,23 @@ public class MyappController {
 		model.addAttribute("getnum", getnum);
 		return "B";
 	}
+	
+	/*
+	 * 문제
+	 * MyappController 파일에 섭씨 온도를 입력할 경우 화씨 온도로 바궈서 출력해주는 페이지를 작성해보도록 하겠습니다.
+	 * 화씨 -> 섭씨 변환 공식은
+	 * 섭씨온도 = (화씨온도 -32) / 1.8입니다.
+	 */
+	
+	@RequestMapping(value="goC", method= {RequestMethod.POST, RequestMethod.GET})
+	public String goC(double celsius,Model model) {
+		double fahrenheit = (celsius - 32) / 1.8;
+		model.addAttribute("f", fahrenheit);
+		return "C";
+	}
+	
+	@RequestMapping(value="ctofform")
+	public String goForm1(Model model) {
+		return "ctofform";
+	}
 }
---
+++ myFirstSpring/src/main/webapp/WEB-INF/views/ctofform.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action ="goC" method = "get">
+		<input type = "number" step="0.1"name = "celsius" required>
+		<input type = "submit" value="온도변환">
+	</form>
+</body>
+</html>
